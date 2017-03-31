91ff66e365cf588097b2cc5f8a23367278b9a003
Alexandra Frock <hosted@alexandra.moe>
2017-03-30 22:23:50 -0400

    Update Sub Adder
    Add a warning if the user doesn't own all of the apps in a sub. If a user does own all of the apps, but not the sub, it warns about that instead. If both of those are satisfied, don't display anything.

--- subadder.user.js
+++ subadder.user.js
@@ -24,42 +24,49 @@
     
     var id = GM_getValue('BarterID', 'none');
     
-    var owned = false;
+    var warning = "";
 
     var type = window.location.href.split("/")[2] == "store.steampowered.com" ? "steam" : "steamdb";
+    
+    if( type == "steamdb" && document.getElementById("apps") === null ) return;
     
     GM_xmlhttpRequest({
         method: "GET",
         url: window.location.href.split("/")[0] + "//store.steampowered.com/dynamicstore/userdata",
         onload: function(resp){
+            
+            var userdata = JSON.parse(resp.responseText);
+            if( userdata.rgOwnedPackages.indexOf(parseInt(window.location.href.split("?")[0].split("/")[4])) == -1 ) warning="you do not own this package";
+            
             var items = [];
             
             if( type == "steam" ){
                 Array.prototype.slice.call(document.getElementsByClassName("tab_item_overlay")).forEach(function(v){
                     items.push(v.href.split("/")[4]);
                 });
-            } else if( type == "steamdb" && document.getElementById("apps") !== null ){
+            } else if( type == "steamdb" ){
                 Array.prototype.slice.call(document.getElementById("apps").children[1].children[1].children).forEach(function(v,k){
                     items.push(v.getAttribute("data-appid"));
                 });
             }
             
-            items = items.join(",");
+            items.forEach(function(v){
+               if( userdata.rgOwnedApps.indexOf(parseInt(v)) == -1 ) warning="you do not own all of the apps in this package";
+            });
             
-            var userdata = JSON.parse(resp.responseText);
-            owned = userdata.rgOwnedPackages.indexOf(parseInt(window.location.href.split("?")[0].split("/")[4])) > -1;
+            items = items.join(",");
 
             var formTemplate = "<form action='https://barter.vg/u/" + id + "/l/e/#modified' method='POST' id='barterPOSTform'><input type='hidden' name='bulk_AppIDs' value='" + items + "'></input>{{button}}<input type='hidden' name='action' value='Edit'></input><input type='hidden' name='change_attempted' value=1></input><input type='hidden' name='add_from' value='AppIDs'></input></form>";
             
             if( type == "steam" ){
                 var button = "<input type='submit' class='btnv6_blue_hoverfade btn_medium' value='Add this sub to your Barter.vg library'";
-                if( ! owned ) button+=" data-store-tooltip='<b style=\"color: red;\">Warning: you do not own this sub.</span>'";
+                if( warning.length > 0 ) button+=" data-store-tooltip='<b style=\"color: red;\">Warning: " + warning + "</span>'";
                 button += "></input>";
                 document.getElementsByClassName("block responsive_apppage_details_right")[0].innerHTML+=formTemplate.replace("{{button}}", button);
                 BindStoreTooltip($J(".btn_medium"));
             } else if( type == "steamdb" ){
                 var button = "<a href='#' onClick='document.getElementById(\"barterPOSTform\").submit();'";
-                if( ! owned ) button+=" class='tooltipped' aria-label='Warning: you do not own this sub'";
+                if( warning.length > 0 ) button+=" class='tooltipped' aria-label='Warning: " + warning + "'";
                 document.getElementsByClassName("pagehead-actions")[0].innerHTML+=button + ">Add this sub to your Barter.vg library</a>" + formTemplate.replace("{{button}}", "");
             }
         }
